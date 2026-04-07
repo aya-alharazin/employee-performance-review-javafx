4bf36d116531b21f8545895dd260756d8a79cf85
Aya <alharazinaya@gmail.com>
2026-04-07 20:44:07 +0200

    First commit

---
+++ Test.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week6;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author aya
+ */
+public class Test extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent p = FXMLLoader.load(getClass().getResource("test.fxml"));
+        Scene s=new Scene(p);
+        stage.setScene(s);
+        stage.show();
+    }
+    
+}
